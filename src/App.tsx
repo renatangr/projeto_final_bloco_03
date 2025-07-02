76332d41fcddc5811da3c17fa9eeaa36e2f64d0b
Renata Negrini <renatanegrinim@gmail.com>
2025-07-02 10:16:47 -0300

    feat: Implementação das rotas em App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
 import Home from './pages/home/Home';
+import ListaCategorias from './components/categorias/listacategorias/ListaCategorias';
+import DeletarCategoria from './components/categorias/deletarcategoria/DeletarCategoria';
+import FormCategoria from './components/categorias/formcategoria/FormCategoria';
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Home />
-      <Footer />
+      <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/categorias" element={<ListaCategorias />} />
+              <Route path="/cadastrarcategoria" element={<FormCategoria />} />
+              <Route path="/editarcategoria/:id" element={<FormCategoria />} />
+              <Route path="/deletarcategoria/:id" element={<DeletarCategoria />} />
+              <Route path="/categorias/:tipo" element={<DeletarCategoria />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
     </>
   );
 }
